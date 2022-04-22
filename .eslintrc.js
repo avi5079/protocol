d262e1d4a2ec8c938a70ae9e633bd7fcec9f69b1
Aviral Srivastava <aviral.srivastava2102@gmail.com>
2022-04-22 19:25:52 +0530

    fixed repeating background in input element

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,74 +1,72 @@
 module.exports = {
     env: {
-        'node': true,
-        'commonjs': true,
-        'jasmine': true,
-        'es2017': true
+        node: true,
+        commonjs: true,
+        jasmine: true,
+        es2017: true,
     },
-    extends: [
-        'eslint:recommended'
-    ],
+    extends: ["eslint:recommended"],
     rules: {
         // Require `let` or `const` instead of `var`
         // https://eslint.org/docs/rules/no-var
-        'no-var': 'error',
+        "no-var": "error",
 
         // Require `const` declarations for variables that are never reassigned after declared
         // https://eslint.org/docs/rules/prefer-const
-        'prefer-const': 'error',
+        "prefer-const": "error",
 
         // This option sets a specific tab width for your code
         // https://eslint.org/docs/rules/indent
-        'indent': ['error', 4],
+        indent: ["error", 4],
 
         // Disallow mixed 'LF' and 'CRLF' as linebreaks
         // https://eslint.org/docs/rules/linebreak-style
-        'linebreak-style': ['error', 'unix'],
+        "linebreak-style": ["error", "windows"],
 
         // Specify whether double or single quotes should be used
-        'quotes': ['error', 'single'],
+        quotes: ["error", "single"],
 
         // Require or disallow use of semicolons instead of ASI
-        'semi': ['error', 'always'],
+        semi: ["error", "always"],
 
         // Enforce location of semicolons
         // https://eslint.org/docs/rules/semi-style
-        'semi-style': ['error', 'last'],
+        "semi-style": ["error", "last"],
 
         // Require camel case names
         // https://eslint.org/docs/rules/camelcase
-        'camelcase': ['error', { 'properties': 'always' }],
+        camelcase: ["error", { properties: "always" }],
 
         // Use type-safe equality operators
         // https://eslint.org/docs/rules/eqeqeq
-        'eqeqeq': ['error', 'always'],
+        eqeqeq: ["error", "always"],
 
         // Require newlines around variable declarations
         // https://eslint.org/docs/rules/one-var-declaration-per-line
-        'one-var-declaration-per-line': ['error', 'always'],
+        "one-var-declaration-per-line": ["error", "always"],
 
         // Require constructor names to begin with a capital letter
         // https://eslint.org/docs/rules/new-cap
-        'new-cap': 'error',
+        "new-cap": "error",
 
         // Disallow Use of alert, confirm, prompt
         // https://eslint.org/docs/rules/no-alert
-        'no-alert': 'error',
+        "no-alert": "error",
 
         // Disallow eval()
         // https://eslint.org/docs/rules/no-eval
-        'no-eval': 'error',
+        "no-eval": "error",
 
         // Disallow empty functions
         // https://eslint.org/docs/rules/no-empty-function
-        'no-empty-function': 'error',
+        "no-empty-function": "error",
 
         // Require radix parameter
         // https://eslint.org/docs/rules/radix
-        'radix': 'error',
+        radix: "error",
 
         // Disallow the use of `console`
         // https://eslint.org/docs/rules/no-console
-        'no-console': 'error'
-    }
+        "no-console": "error",
+    },
 };
